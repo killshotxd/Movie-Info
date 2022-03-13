f5af2e5ba0eedd3c69ae550192f309f4d3f65d49
killshotxd <hassanansari211@gmail.com>
2022-03-13 09:22:31 +0530

    implemented search via API

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const fetchData = async () => {
-  const response = await axios.get();
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "3fb94382",
+      s: searchTerm,
+    },
+  });
+  console.log(response.data);
 };
+
+const input = document.querySelector("input");
+
+const onInput = (e) => {
+  fetchData(e.target.value);
+};
+input.addEventListener("input", debounce(onInput, 500));
